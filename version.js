80c1e6db23267331e85710cf2d15166d1a3bfcb7
Ryan McCaw <96198826+rtkrmccaw@users.noreply.github.com>
2022-11-01 18:01:05 -0400

    Fixed logic to match description and updated dependencies (#38)
    - Updated node runtime from `12` -> `16`
    - Fixed `getReleaseNotes`
      - I expect that with a prefix set, a message without that prefix does
        not have release notes
      - Either return an empty string or throw an error if a prefix is
        defined AND that prefix doesn't appear in the PR body
        - Same for if a suffix is defined
    
    Behaviour before with `release-notes-prefix` set to `--begin--`
    ```
    My PR description
    ```
    The release notes for this would be "My PR description"
    ```
    My PR description
    --begin--
    ```
    The release notes for this would be ""
    
    With this change they now act the same. Both examples will return "" or
    throw an error if `require-release-notes` is `true`

--- version.js
+++ version.js
@@ -1,116 +1,131 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const semver = require('semver')
-const childProcess = require('child_process')
-const fs = require('fs')
+const core = require("@actions/core");
+const github = require("@actions/github");
+const semver = require("semver");
+const childProcess = require("child_process");
+const fs = require("fs");
 
-const DEFAULT_VERSION = '0.0.0'
+const DEFAULT_VERSION = "0.0.0";
 
 async function getCommitsOnBranch(branch, config) {
-    const commits = new Set()
-    // eslint-disable-next-line no-restricted-syntax
-    for await (const response of config.octokit.paginate.iterator(
-        config.octokit.rest.repos.listCommits,
-        {
-            ...github.context.repo,
-            sha: branch,
-        },
-    )) {
-        response.data.forEach((commit) => {
-            commits.add(commit.sha)
-        })
+  const commits = new Set();
+  // eslint-disable-next-line no-restricted-syntax
+  for await (const response of config.octokit.paginate.iterator(
+    config.octokit.rest.repos.listCommits,
+    {
+      ...github.context.repo,
+      sha: branch,
     }
-    return commits
+  )) {
+    response.data.forEach((commit) => {
+      commits.add(commit.sha);
+    });
+  }
+  return commits;
 }
 
-async function getLatestVersionInCommits(commits, sortedVersions, objectsByVersion, config) {
-    for (let i = 0; i < sortedVersions.length; i++) {
-        const refObj = objectsByVersion[sortedVersions[i]]
+async function getLatestVersionInCommits(
+  commits,
+  sortedVersions,
+  objectsByVersion,
+  config
+) {
+  for (let i = 0; i < sortedVersions.length; i++) {
+    const refObj = objectsByVersion[sortedVersions[i]];
 
-        if (refObj.type === 'commit' && commits.has(refObj.sha)) {
-            return `${sortedVersions[i]}`
-        }
-
-        if (refObj.type === 'tag') {
-            // eslint-disable-next-line no-await-in-loop
-            const tag = await config.octokit.rest.git.getTag({
-                ...github.context.repo,
-                tag_sha: refObj.sha,
-            })
-
-            if (commits.has(tag.data.object.sha)) {
-                return `${sortedVersions[i]}`
-            }
-        }
+    if (refObj.type === "commit" && commits.has(refObj.sha)) {
+      return `${sortedVersions[i]}`;
     }
 
-    return DEFAULT_VERSION
+    if (refObj.type === "tag") {
+      // eslint-disable-next-line no-await-in-loop
+      const tag = await config.octokit.rest.git.getTag({
+        ...github.context.repo,
+        tag_sha: refObj.sha,
+      });
+
+      if (commits.has(tag.data.object.sha)) {
+        return `${sortedVersions[i]}`;
+      }
+    }
+  }
+
+  return DEFAULT_VERSION;
 }
 
 // Tags the specified version and annotates it with the provided release notes.
 async function createRelease(version, releaseNotes, config) {
-    const tag = `${config.v}${version}`
+  const tag = `${config.v}${version}`;
 
-    if (!config.useSSH) {
-        const tagCreateResponse = await config.octokit.git.createTag({
-            ...github.context.repo,
-            tag: tag,
-            message: releaseNotes,
-            object: process.env.GITHUB_SHA,
-            type: 'commit',
-        })
+  if (!config.useSSH) {
+    const tagCreateResponse = await config.octokit.rest.git.createTag({
+      ...github.context.repo,
+      tag: tag,
+      message: releaseNotes,
+      object: process.env.GITHUB_SHA,
+      type: "commit",
+    });
 
-        await config.octokit.git.createRef({
-            ...github.context.repo,
-            ref: `refs/tags/${tag}`,
-            sha: tagCreateResponse.data.sha,
-        })
-    } else {
-        const releaseNotesFile = './.relase-notes.txt'
-        fs.writeFileSync(releaseNotesFile, releaseNotes)
-        // The git checkout is already on the correct commit, simply
-        // add a tag to add the /refs/tags/...
-        childProcess.execSync(`git tag -F ${releaseNotesFile} ${tag}`)
-        childProcess.execSync('git push --tags')
-    }
+    await config.octokit.rest.git.createRef({
+      ...github.context.repo,
+      ref: `refs/tags/${tag}`,
+      sha: tagCreateResponse.data.sha,
+    });
+  } else {
+    const releaseNotesFile = "./.release-notes.txt";
+    fs.writeFileSync(releaseNotesFile, releaseNotes);
+    // The git checkout is already on the correct commit, simply
+    // add a tag to add the /refs/tags/...
+    childProcess.execSync(`git tag -F ${releaseNotesFile} ${tag}`);
+    childProcess.execSync("git push --tags");
+  }
 
-    return tag
+  return tag;
 }
 
 // Returns the most recent tagged version in git.
 async function getCurrentVersion(config) {
-    const data = await config.octokit.git.listMatchingRefs({
-        ...github.context.repo,
-        ref: 'tags/',
-    })
+  const data = await config.octokit.rest.git.listMatchingRefs({
+    ...github.context.repo,
+    ref: "tags/",
+  });
 
-    const objectsByVersion = new Map()
-    const versions = []
+  const objectsByVersion = new Map();
+  const versions = [];
 
-    data.data.forEach((ref) => {
-        const version = semver.parse(ref.ref.replace(/^refs\/tags\//g, ''), { loose: true })
+  data.data.forEach((ref) => {
+    const version = semver.parse(ref.ref.replace(/^refs\/tags\//g, ""), {
+      loose: true,
+    });
 
-        if (version !== null) {
-            objectsByVersion[version] = ref.object
-            versions.push(version)
-        }
-    })
-
-    versions.sort(semver.rcompare)
-
-    if (config.baseBranch) {
-        const branch = process.env.GITHUB_BASE_REF || (process.env.GITHUB_REF && process.env.GITHUB_REF.replace('refs/heads/', ''))
-        core.info(`Only considering tags on branch ${branch}`)
-        const commits = await getCommitsOnBranch(branch, config)
-        return getLatestVersionInCommits(commits, versions, objectsByVersion, config)
+    if (version !== null) {
+      objectsByVersion[version] = ref.object;
+      versions.push(version);
     }
+  });
 
-    if (versions[0] !== undefined) {
-        return `${versions[0]}`
-    }
+  versions.sort(semver.rcompare);
 
-    return DEFAULT_VERSION
+  if (config.baseBranch) {
+    const branch =
+      process.env.GITHUB_BASE_REF ||
+      (process.env.GITHUB_REF &&
+        process.env.GITHUB_REF.replace("refs/heads/", ""));
+    core.info(`Only considering tags on branch ${branch}`);
+    const commits = await getCommitsOnBranch(branch, config);
+    return getLatestVersionInCommits(
+      commits,
+      versions,
+      objectsByVersion,
+      config
+    );
+  }
+
+  if (versions[0] !== undefined) {
+    return `${versions[0]}`;
+  }
+
+  return DEFAULT_VERSION;
 }
 
-exports.createRelease = createRelease
-exports.getCurrentVersion = getCurrentVersion
+exports.createRelease = createRelease;
+exports.getCurrentVersion = getCurrentVersion;
